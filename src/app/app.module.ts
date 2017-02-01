6980851981002b021b37bdaf5b1a9dbbdacb77c5
nikohol <nikohol@metropolia.fi>
2017-02-01 13:34:22 +0200

    inxex commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CommentComponent } from './comment/comment.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    LoginComponent,
+    RegisterComponent,
+    CommentComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
